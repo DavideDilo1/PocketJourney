46d4af97a722d911b0d27b9c3206a926d05a5d01
Davide <thedavorgame@gmail.com>
2023-05-25 21:38:32 +0200

    Activity login che teoricamente funziona

--- app/src/main/java/com/example/pocketjourney/MainActivity.kt
+++ app/src/main/java/com/example/pocketjourney/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.pocketjourney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.pocketjourney.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            //Toast.makeText(this,"Email:"+ binding.etEmailLogin.toString(),Toast.LENGTH_SHORT)
+          //  Toast.makeText(this,"Password:"+ binding.etPasswordLogin.toString(),Toast.LENGTH_SHORT)
+            binding.etEmailLogin.setText("suca")
+        }
     }
 }
