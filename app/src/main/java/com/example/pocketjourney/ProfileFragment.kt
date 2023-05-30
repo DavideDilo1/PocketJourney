36dcba2dc112c614293237dbe4eed58ba97f11e9
Davide <thedavorgame@gmail.com>
2023-05-30 09:34:42 +0200

    Prima bozza profilo

--- app/src/main/java/com/example/pocketjourney/ProfileFragment.kt
+++ app/src/main/java/com/example/pocketjourney/ProfileFragment.kt
@@ -4,21 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.pocketjourney.databinding.FragmentProfileBinding
+import com.example.pocketjourney.databinding.FragmentRegistrationBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding= FragmentProfileBinding.inflate(layoutInflater,container,false)
+
+    /*    binding.btnModifica.setOnClickListener{
+            //mi porta a fragment modifica dati
+        }*/
+
+    /*    binding.btnLogout.setOnClickListener {
+        }*/
+
+        return binding.root
     }
 
 
